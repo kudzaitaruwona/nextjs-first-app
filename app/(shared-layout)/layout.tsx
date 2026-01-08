09b11ef518054edea6cb3768da1212a284e36dfc
Kudzai Taruwona <ketaruwona@gmail.com>
2026-01-08 22:08:02 +0200

    1st update

---
+++ app/(shared-layout)/layout.tsx
@@ -0,0 +1,12 @@
+import { Navbar } from "@/components/web/navbar";
+import { ReactNode } from "react";
+
+export default function SharedLayout({children}: {children:ReactNode}){
+
+    return(
+        <>
+            <Navbar/>
+            {children}
+        </>
+    )
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,10 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Navbar } from "@/components/web/navbar";
+import { ThemeProvider } from "@/components/ui/theme-provider";
+import { ConvexClientProvider } from "@/components/web/ConvexClientProvider";
+import { Toaster } from "@/components/ui/sonner";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,11 +27,19 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+    <html lang="en" suppressHydrationWarning>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`} suppressHydrationWarning >
+        <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+        <main className="max-w-7xl mx-auto w-full px-4 md:px-6 lg:px-8">
+          <ConvexClientProvider>{children}</ConvexClientProvider>
+        </main>
+        <Toaster closeButton />
+        </ThemeProvider>
       </body>
     </html>
   );
